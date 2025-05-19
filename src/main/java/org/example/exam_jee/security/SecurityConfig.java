ab9fe7b71e642338b21ca64a1f7d75e42795c8c6
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-05-19 11:07:03 +0100

    Backend security commit

---
+++ src/main/java/org/example/exam_jee/security/SecurityConfig.java
@@ -0,0 +1,110 @@
+package org.example.exam_jee.security;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        if (!manager.userExists("admin")) {
+            manager.createUser(User.withUsername("admin")
+                    .password(passwordEncoder().encode("admin123"))
+                    .roles("ADMIN")
+                    .build());
+        }
+
+        if (!manager.userExists("employe")) {
+            manager.createUser(User.withUsername("employe")
+                    .password(passwordEncoder().encode("emp123"))
+                    .roles("EMPLOYE")
+                    .build());
+        }
+
+        if (!manager.userExists("client")) {
+            manager.createUser(User.withUsername("client")
+                    .password(passwordEncoder().encode("cli123"))
+                    .roles("CLIENT")
+                    .build());
+        }
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf->csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2ResourceServer(oa->oa.jwt(Customizer.withDefaults()))
+                .build();
+    }
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        return new NimbusJwtEncoder(new ImmutableSecret<>(key));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(key).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        ///corsConfiguration.setExposedHeaders(List.of("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return source;
+
+    }
+}
--- src/main/java/org/example/exam_jee/web/GestionCreditController.java
+++ src/main/java/org/example/exam_jee/web/GestionCreditController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.exam_jee.dtos.*;
 import org.example.exam_jee.services.GestionCreditService;
 import org.example.exam_jee.dtos.ClientDTO;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,71 +19,85 @@ public class GestionCreditController {
 
     private final GestionCreditService service;
     @PostMapping("/clients")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public ClientDTO saveClient(@RequestBody ClientDTO dto) {
         return service.saveClient(dto);
     }
 
     @GetMapping("/clients/{id}")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public ClientDTO getClientById(@PathVariable Long id) {
         return service.getClientById(id);
     }
 
     @GetMapping("/clients")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public List<ClientDTO> getAllClients() {
         return service.getAllClients();
     }
 
     @PutMapping("/clients/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ClientDTO updateClient(@PathVariable Long id, @RequestBody ClientDTO dto) {
         return service.updateClient(id, dto);
     }
 
     @DeleteMapping("/clients/{id}")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public void deleteClient(@PathVariable Long id) {
         service.deleteClient(id);
     }
 
     @PostMapping("/credits/personnel")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public CreditPersonnelDTO saveCreditPersonnel(@RequestBody CreditPersonnelDTO dto) {
         return service.saveCreditPersonnel(dto);
     }
 
     @PostMapping("/credits/immobilier")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public CreditImmobilierDTO saveCreditImmobilier(@RequestBody CreditImmobilierDTO dto) {
         return service.saveCreditImmobilier(dto);
     }
 
     @PostMapping("/credits/professionnel")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public CreditProfessionnelDTO saveCreditProfessionnel(@RequestBody CreditProfessionnelDTO dto) {
         return service.saveCreditProfessionnel(dto);
     }
 
     @PostMapping("/remboursements")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public RemboursementDTO saveRemboursement(@RequestBody RemboursementDTO dto) {
         return service.saveRemboursement(dto);
     }
 
     @GetMapping("/remboursements/{id}")
+    @PreAuthorize("hasAuthority(SCOPE_ADMIN)")
     public RemboursementDTO getRemboursementById(@PathVariable Long id) {
         return service.getRemboursementById(id);
     }
 
     @GetMapping("/remboursements")
+    @PreAuthorize("hasAnyRole('CLIENT', 'EMPLOYE', 'ADMIN')")
     public List<RemboursementDTO> getAllRemboursements() {
         return service.getAllRemboursements();
     }
 
     @GetMapping("/credits/{creditId}/remboursements")
+    @PreAuthorize("hasAnyRole('CLIENT', 'EMPLOYE', 'ADMIN')")
     public List<RemboursementDTO> getRemboursementsByCreditId(@PathVariable Long creditId) {
         return service.getRemboursementsByCreditId(creditId);
     }
 
     @PutMapping("/remboursements/{id}")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public RemboursementDTO updateRemboursement(@PathVariable Long id, @RequestBody RemboursementDTO dto) {
         return service.updateRemboursement(id, dto);
     }
 
     @DeleteMapping("/remboursements/{id}")
+    @PreAuthorize("hasAnyRole('EMPLOYE', 'ADMIN')")
     public void deleteRemboursement(@PathVariable Long id) {
         service.deleteRemboursement(id);
     }
